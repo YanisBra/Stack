57c2945943d219d26f54a2e54f4edd66683b78f1
Yanis Brahimi <yanisb@Ordinateur-portable-de-Yanis.local>
2024-02-06 13:40:44 +0100

    first commit

---
+++ src/Entity/Member.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MemberRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MemberRepository::class)]
+class Member extends User
+{
+    //#[ORM\Id]
+    //#[ORM\GeneratedValue]
+    //#[ORM\Column]
+    //private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $registration_date = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $biography = null;
+
+    #[ORM\Column]
+    private ?bool $validated = null;
+
+    #[ORM\Column]
+    private ?int $reputation = null;
+
+    // public function getId(): ?int
+    // {
+    //     return $this->id;
+    // }
+
+    public function getRegistrationDate(): ?\DateTimeInterface
+    {
+        return $this->registration_date;
+    }
+
+    public function setRegistrationDate(\DateTimeInterface $registration_date): static
+    {
+        $this->registration_date = $registration_date;
+
+        return $this;
+    }
+
+    public function getBiography(): ?string
+    {
+        return $this->biography;
+    }
+
+    public function setBiography(?string $biography): static
+    {
+        $this->biography = $biography;
+
+        return $this;
+    }
+
+    public function isValidated(): ?bool
+    {
+        return $this->validated;
+    }
+
+    public function setValidated(bool $validated): static
+    {
+        $this->validated = $validated;
+
+        return $this;
+    }
+
+    public function getReputation(): ?int
+    {
+        return $this->reputation;
+    }
+
+    public function setReputation(int $reputation): static
+    {
+        $this->reputation = $reputation;
+
+        return $this;
+    }
+}
